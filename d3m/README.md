97006dbf8e8a25fe87e139f03a3e2864b4810b47
alarcj <alarcj137@gmail.com>
2017-08-23 11:06:56 -0500

    first commit

---
+++ d3m/README.md
@@ -0,0 +1,16 @@
+# Deploying primitives as Docker containers
+
+1. Modify `process_msg` inside of `kafka_wrapper.py` to process each incoming
+request.
+
+2. Modify `environment.env` to have the correct environmental variables.
+
+3. Build and run container as,
+```
+sudo docker build -t <name> . && sudo docker run -d --env-file environment.env <name>
+```
+
+4. Double check it went well
+```
+sudo docker ps
+```
---
+++ d3m/kafka_wrapper/kafka_wrapper.py
@@ -0,0 +1,32 @@
+import os
+from kafka import KafkaProducer, KafkaConsumer
+
+
+def process_msg(msg):
+    return "Hello World"
+
+
+if __name__=="__main__":
+    # Connect to Kafka via env vars
+    kafka_server  = os.environ.get(KAFKASERVER)
+    consumerTopic = os.environ.get(CONSUMERTOPIC)
+    producerTopic = os.environ.get(PRODUCERTOPIC)
+    assert kafka_server is not None
+    assert consumerTopic is not None 
+    assert producerTopic is not None
+
+    # Create producer and consumer
+    consumer = KafkaConsumer(consumerTopic, bootstrap_servers=[kafka_server])
+    producer = KafkaProducer(bootstrap_servers=kafka_server)
+    for msg in consumer:
+        # Read incoming msg
+        incomingMsg = msg.value.decode('utf-8')
+
+        # process incomgin msg and produce outgoing msg
+        outgoing = process_msg(incomingMsg)
+
+        # Send outgoing msg
+        outgoingMsg = producer.send(producerTopic, outgoing.encode('utf-8'))
+
+    producer.flush()
+
