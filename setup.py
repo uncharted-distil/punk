bfcb71678c549d9458b38b8cc78de0b5663135f0
alarcj <alarcj137@gmail.com>
2017-09-07 12:54:36 -0500

    rm fa from feature selection

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="punk",
 
-    version="1.0.0",
+    version="1.0.1",
 
     description="Primitives for Uncovering New Knowledge.",
     long_description="Machine Learning pipeline elements.",
