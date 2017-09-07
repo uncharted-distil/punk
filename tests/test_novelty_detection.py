0f5a9e28319f304000a047e84c032bca1e69bff9
alarcj <alarcj137@gmail.com>
2017-09-07 12:22:24 -0500

    examples conform to d3m api

--- tests/test_novelty_detection.py
+++ tests/test_novelty_detection.py
@@ -20,10 +20,11 @@ class TestHetero(unittest.TestCase):
     def test_hetero(self):
         hetero = HeteroscedasticityTest(max_iter=1000, tol=0.01)
         hetero = hetero.fit(["matrix"], self.X_hetero)
+        res = hetero.transform()
 
-        self.assertTrue( hetero.fa[0] > -80 and hetero.fa[0] < -70 )
-        self.assertTrue( hetero.fa[1] == 10 )
-        self.assertTrue( hetero.pca[0] > -80 and hetero.pca[0] < -70)
-        self.assertTrue( hetero.pca[1] >= 40 )
+        self.assertTrue( res["fa"][0] > -80 and res["fa"][0] < -70 )
+        self.assertTrue( res["fa"][1] == 10 )
+        self.assertTrue( res["pca"][0] > -80 and res["pca"][0] < -70)
+        self.assertTrue( res["pca"][1] >= 40 )
         self.assertTrue( np.all(np.isfinite(hetero.pca_scores)) )
         self.assertTrue( np.all(np.isfinite(hetero.fa_scores)) )
